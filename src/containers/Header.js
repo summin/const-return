11a703f0f6355e2a73ab3a1af44f446bccfbfc8d
Aleksandr Leontjev <alexander.leontjev@gmail.com>
2019-08-13 14:21:36 +0200

    update

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import Logo from '../media/images/Logo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
+
 
 export default class Header extends Component {
 
     render() {
+
 
         return (
             <div className="nav">
@@ -14,11 +18,11 @@ export default class Header extends Component {
                     <li>Motivation</li>
                     <li>About</li>
                 </ul>
-
-                <ul className = "dropdown account nav">
+                <FontAwesomeIcon icon={faUserCircle}/>
+                <ul className="dropdown account nav">
                     Account
                     <li>
-                        
+
                     </li>
                 </ul>
             </div>
