c86fc2923b29833744c5c8d25673e423d1bfe3fa
Aleksandr Leontjev <alexander.leontjev@gmail.com>
2019-08-15 16:45:06 +0200

    dev

--- src/containers/content/ContentHero.js
+++ src/containers/content/ContentHero.js
@@ -3,15 +3,23 @@ import React, { Fragment, Component } from 'react';
 export default class ContentHero extends Component {
 
     render() {
+        const wordA = "Const"
+        const wordB = "return"
+
+        const wordSlice = (word) => {
+            let chars = word.split("")
+            return chars
+        }
+
 
         return (
             <div className="text-centre hero">
-                hero <br></br>
+                <br></br>
                 <div className="hero-a">
-                    Const
+                {wordSlice(wordA).map((i) => <span className="heroWordA" key={"heroWordA" + i}>{i}</span>)}
                 </div>
                 <div className="hero-b">
-                    return
+                {wordSlice(wordB).map((i) => <span className="heroWordB" key={"heroWordB" + i}>{i}</span>)}
                 </div>
             </div>
         );
